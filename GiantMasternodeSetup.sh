c7104e1d59ae7739df1871979ec5e126bbfa6cb0
GiantCoreDev <dev@giantpay.network>
2018-05-18 22:15:20 +0700

    * Added actual update script

--- GiantMasternodeSetup.sh
+++ GiantMasternodeSetup.sh
@@ -14,7 +14,7 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get install nano htop git -y
 
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common libzmq5-dev libminiupnpc-dev unzip -y
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common python-software-properties libzmq5-dev libminiupnpc-dev unzip -y
 sudo apt-get install libboost-all-dev -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
---
+++ GiantMasternodeUpdate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\n\nsetup giantd ...\n\n"
+cd /root/giant
+wget https://github.com/GiantPay/GiantCore/releases/download/1.1.0.1/giant-1.1.0.1-linux64.zip
+chmod -R 755 /root/giant-1.1.0.1-linux64.zip
+unzip -o giant-1.1.0.1-linux64.zip
+sleep 5
+rm /root/giant/giant-qt
+rm /root/giant/giant-tx
+rm /root/giant/giant-1.1.0.1-linux64.zip
+
+echo -e "\n\nlaunch giantd ...\n\n"
+./giant-cli stop
+./giantd -daemon
+
+echo "GIANT Masternode updated"
+
