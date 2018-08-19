103132722ca5963ce4f5ab202142f76183d64dd3
GiantCoreDev <42275280+GiantCoreDev@users.noreply.github.com>
2018-08-20 06:02:21 +0700

    Update GiantMasternodeUpdate.sh

--- GiantMasternodeUpdate.sh
+++ GiantMasternodeUpdate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo -e "\n\nsetup giantd ...\n\n"
+echo -e "\n\nupdate giantd ...\n\n"
 cd /root/giant
-wget https://github.com/GiantPay/GiantCore/releases/download/1.1.0.3/giant-1.1.0.3-linux64.zip
-chmod -R 755 /root/giant-1.1.0.3-linux64.zip
-unzip -o giant-1.1.0.3-linux64.zip
+wget https://github.com/GiantPay/GiantCore/releases/download/1.2.0.0/giant-1.2.0.0-linux64.zip
+chmod -R 755 /root/giant-1.2.0.0-linux64.zip
+unzip -o giant-1.2.0.0-linux64.zip
 sleep 5
 rm /root/giant/giant-qt
 rm /root/giant/giant-tx
-rm /root/giant/giant-1.1.0.3-linux64.zip
+rm /root/giant/giant-1.2.0.0-linux64.zip
 
 echo -e "\n\nlaunch giantd ...\n\n"
 ./giant-cli stop
