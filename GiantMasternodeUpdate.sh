f56d9899ef3a5c7630d2450f3a36c8ed2c5b91b8
GiantCoreDev <42275280+GiantCoreDev@users.noreply.github.com>
2018-08-20 07:33:34 +0700

    Update GiantMasternodeUpdate.sh

--- GiantMasternodeUpdate.sh
+++ GiantMasternodeUpdate.sh
@@ -2,6 +2,9 @@
 
 echo -e "\n\nupdate giantd ...\n\n"
 cd /root/giant
+./giant-cli stop
+sleep 10
+
 wget https://github.com/GiantPay/GiantCore/releases/download/1.2.0.0/giant-1.2.0.0-linux64.zip
 chmod -R 755 /root/giant-1.2.0.0-linux64.zip
 unzip -o giant-1.2.0.0-linux64.zip
@@ -13,7 +16,6 @@ rm /root/giant/giant-1.2.0.0-linux64.zip
 echo "staking=0" >> /root/.giant/giant.conf
 
 echo -e "\n\nlaunch giantd ...\n\n"
-./giant-cli stop
 ./giantd -daemon -reindex
 
 echo "GIANT Masternode updated"
