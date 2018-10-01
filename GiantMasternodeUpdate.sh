18d6f60050e20b3d3d92202898a53cba45e721e0
GiantCoreDev <42275280+GiantCoreDev@users.noreply.github.com>
2018-10-01 07:20:31 +0700

    Update GiantMasternodeUpdate.sh

--- GiantMasternodeUpdate.sh
+++ GiantMasternodeUpdate.sh
@@ -5,13 +5,13 @@ cd /root/giant
 ./giant-cli stop
 sleep 10
 
-wget https://github.com/GiantPay/GiantCore/releases/download/1.2.2.0/giant-1.2.2.0-linux64.zip
-chmod -R 755 /root/giant/giant-1.2.2.0-linux64.zip
-unzip -o giant-1.2.2.0-linux64.zip
+wget https://github.com/GiantPay/GiantCore/releases/download/1.2.2.1/giant-1.2.2.1-linux64.zip
+chmod -R 755 /root/giant/giant-1.2.2.1-linux64.zip
+unzip -o giant-1.2.2.1-linux64.zip
 sleep 5
 rm /root/giant/giant-qt
 rm /root/giant/giant-tx
-rm /root/giant/giant-1.2.2.0-linux64.zip
+rm /root/giant/giant-1.2.2.1-linux64.zip
 
 echo "staking=0" >> /root/.giant/giant.conf
 
