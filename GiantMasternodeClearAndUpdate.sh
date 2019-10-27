85878dbd4fdcf80040a1d09714eec9edd4aff4d1
filinberg <filinberg@gmail.com>
2019-10-27 12:26:20 +0200

    Update GiantMasternodeClearAndUpdate.sh

--- GiantMasternodeClearAndUpdate.sh
+++ GiantMasternodeClearAndUpdate.sh
@@ -19,13 +19,13 @@ if [ -d "$GIANT_DATADIR" ]; then
   cp $GIANT_DATADIR_BACKUP/wallet.dat $GIANT_DATADIR/wallet.dat
 fi
 
-wget https://github.com/GiantPay/GiantCore/releases/download/1.4.0.0/giant-1.4.0-x86_64-linux-gnu.tar.gz
-chmod -R 755 /root/giant/giant-1.4.0-x86_64-linux-gnu.tar.gz
-tar -xzf giant-1.4.0-x86_64-linux-gnu.tar.gz
+wget https://github.com/GiantPay/GiantCore/releases/download/1.4.1/giant-1.4.1-x86_64-linux-gnu.tar.gz
+chmod -R 755 /root/giant/giant-1.4.1-x86_64-linux-gnu.tar.gz
+tar -xzf giant-1.4.1-x86_64-linux-gnu.tar.gz
 sleep 5
 
-cp -f $GIANT_PATH/giant-1.4.0/bin/giantd $GIANT_PATH
-cp -f $GIANT_PATH/giant-1.4.0/bin/giant-cli $GIANT_PATH
+cp -f $GIANT_PATH/giant-1.4.1/bin/giantd $GIANT_PATH
+cp -f $GIANT_PATH/giant-1.4.1/bin/giant-cli $GIANT_PATH
 rm -Rf $GIANT_PATH/giant-1.*
 
 echo -e "\n\nlaunch giantd ...\n\n"
